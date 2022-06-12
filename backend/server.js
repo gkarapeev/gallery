36465872b8f90ad99f76df2dc461a3b9a87eabe0
karapeev <karapeev@progress.com>
2022-06-12 19:32:51 +0300

    feat: add simple mock UI

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 
 // Routes
 app.get('/', (req, res) => {
-    console.log('The server was accessed.')
+    console.log('The server was accessed.');
 });
+
+// TODO: sort the files to ensure consistent order every time
 
 app.listen(3000);
