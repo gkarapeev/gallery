0d3145b2ee9d016195ee719d2dd6656b8101ccfc
karapeev <karapeev@progress.com>
2022-06-13 07:52:48 +0300

    feat: draft virtualization

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,6 +1,22 @@
+// Elements
 const containerElement = document.getElementById('container');
 const contentElement = document.getElementById('content');
 const spacerElement = document.getElementById('spacer');
+
+// Variables
+const cardHeight = 200;
+const gapHeight = 20;
+const rowHeight = cardHeight + gapHeight;
+const containerHeight = parseInt(window.getComputedStyle(containerElement).height.split('px')[0]);
+const itemsPerPage = 16;
+const visibleCardCount = itemsPerPage * 3;
+
+let skip = 0;
+
+let totalImgCount;
+let totalRowCount;
+
+// Functions
 function makeCard(url) {
     const cardElement = document.createElement('li');
     cardElement.setAttribute('class', 'card');
@@ -15,44 +31,78 @@ function makeCard(url) {
     return cardElement;
 }
 
-const cardHeight = 200;
-const gapHeight = 20;
-const rowHeight = cardHeight + gapHeight;
-const containerHeight = parseInt(window.getComputedStyle(containerElement).height.split('px')[0]);
-const visibleCardCount = 48;
 
-let totalImgCount;
-let totalRowCount;
+// Element Map
+let renderedPages = [0, 1, 2];
 
-function loadInitialImages() {
-    const req = new XMLHttpRequest();
-
-    // TODO: dispose of listener
-    req.addEventListener('readystatechange', () => {
-        if (req.readyState === 4) {
-            const response = JSON.parse(req.response);
-
-            totalImgCount = response.total;
-            totalRowCount = totalImgCount / 4;
-            spacerElement.style.height = totalRowCount * rowHeight + 'px';
-
-            const urls = response.results;
-
-            for (let i=0; i <= visibleCardCount - 1; i++) {
-                contentElement.appendChild(makeCard(urls[i]));
-            }
-        }
-    });
-
-    req.open('GET', 'http://localhost:3000/images');
-    req.send();
+// Init card nodes
+for (let i=0; i <= visibleCardCount - 1; i++) {
+    contentElement.appendChild(makeCard('img/loading.png'));
 }
 
-loadInitialImages();
+// Fetch total
+fetch('http://localhost:3000/image-count')
+    .then(res => res.json())
+    .then(total => {
+        totalImgCount = total;
+        totalRowCount = totalImgCount / 4;
+        spacerElement.style.height = totalRowCount * rowHeight + 'px';
+    });
+
+function renderMap() {
+    renderedPages.forEach((pageIndex, orderIndex) => {
+        const pageImages = imagesForPage(pageIndex);
+
+        const elementsForThisGroup = getNodeGroups()[orderIndex];
+
+        elementsForThisGroup.forEach(
+            (node, nodeIndex) => {
+                fetch(urlForImage(pageImages[nodeIndex]))
+                    .then(res => res.json())
+                    .then(imageUrl => {
+                        node.firstChild.setAttribute('src', imageUrl);
+                    });
+            }
+        );
+    });
+}
+
+renderMap();
 
 containerElement.addEventListener('scroll', () => {
     const firstVisibleRowIndex = Math.floor(containerElement.scrollTop / rowHeight);
-    const firstVisibleImageIndex = firstVisibleRowIndex * 4;
+    const firstPageInViewIndex = Math.floor(firstVisibleRowIndex / 4);
 
-    console.log(firstVisibleImageIndex);
+    if (firstPageInViewIndex !== skip) {
+        skip = firstPageInViewIndex;
+        getNodeGroups()[0].forEach(el => { el.style.transform = 'translate(0,' + offsetForPage(3) + 'px'; });
+        renderedPages = renderedPages.map(pageIndex => pageIndex + 1);
+        renderMap();
+        console.log('jump');
+    }
 });
+
+function imagesForPage(pageIndex) {
+    return Array(16).fill().map((_, index) => pageIndex * 16 + index);
+}
+
+function offsetForPage(pageIndex) {
+    return pageIndex * containerElement.getBoundingClientRect().height;
+}
+
+function rotate(arr) {
+    arr.push(arr.shift());
+    return arr;
+}
+
+function getNodeGroups() {
+    return [
+        Array.from(contentElement.children).slice(0, 16),
+        Array.from(contentElement.children).slice(16, 32),
+        Array.from(contentElement.children).slice(32, 48)
+    ]
+}
+
+function urlForImage(imageIndex) {
+    return 'http://localhost:3000/image/' + imageIndex;
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
 const cors = require('cors');
+
 
 
 const app = express();
@@ -11,33 +11,41 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.get('/', () => {
-    
+// Read files
+const imagesDirectory = path.join(__dirname, 'public', 'img', 'downsized');
+let urlList;
+
+fs.readdir(imagesDirectory, (err, files) => {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+
+    const sorted = files.sort(); // Ensure images always arrive in the same order
+
+    urlList = sorted.map(filename => 'img/downsized/' + filename);
 });
+
+// Routes
 app.get('/images', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    const imagesDirectory = path.join(__dirname, 'public', 'img', 'downsized');
-
-
-    fs.readdir(imagesDirectory, (err, files) => {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-
-        const sorted = files.sort();
-
-        const urlList = sorted.map(filename => 'img/downsized/' + filename);
-
-        return res.json({
-            total: files.length,
-            results: urlList
-        })
-    });
+    return res.json({
+        total: files.length,
+        results: urlList
+    })
 });
 
-// TODO: sort the files to ensure consistent order every time
+app.get('/image/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    return res.json(urlList[req.params.id]);
+});
+
+app.get('/image-count', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    return res.json(urlList.length);
+});
 
 app.listen(3000);
 console.log('Server running');
